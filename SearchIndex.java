997e90b569b5a149b09fd115577022808bbee856
DhanuSree731 <2420090101@klh.edu.in>
2025-08-05 08:52:45 +0530

    Add files via upload

---
+++ SearchIndex.java
@@ -0,0 +1,12 @@
+package oop_practical;
+public class SearchIndex 
+{
+ public static void main(String[] args) {
+ String s = "Hello_world";
+        System.out.println(s.indexOf("_"));        
+        System.out.println(s.lastIndexOf("o"));    
+        System.out.println(s.contains("11"));      
+    
+        
+ }
+}
