39cbe7f39d1eff92db2b622fc1ade679cb9c1c7a
DhanuSree731 <2420090101@klh.edu.in>
2025-08-05 14:01:12 +0530

    Add files via upload

---
+++ BoxDemo2.java
@@ -0,0 +1,27 @@
+package oop_practical;
+class Box {
+	double width;
+	double height;
+	double depth;
+}
+public class BoxDemo2 {
+	public static void main(String args[]) {
+		Box mybox1 = new Box();
+		Box mybox2 = new Box();
+		double vol;
+		mybox1.width=10;
+		mybox1.height=20;
+		mybox1.depth=15;
+		
+		mybox2.width=14;
+		mybox2.height=24;
+		mybox2.depth=5;
+		
+		vol=mybox1.width*mybox1.height*mybox1.depth;
+		System.out.println("Volume is :" + vol);
+		
+		vol=mybox2.width*mybox2.height*mybox2.depth;
+		System.out.println("Volume is :" + vol);
+	}
+
+}
